255376aa6e2ad7798517c9853ba9d689b48c9dc0
YuriTob1 <101827742+YuriTob1@users.noreply.github.com>
2022-05-23 19:41:40 -0300

    Add files via upload

---
+++ Tower.js
@@ -0,0 +1,17 @@
+class Tower {
+constructor(x,y,w,h){
+var options = {
+ isStatic:true
+}
+this.w = w;
+this.h = h;
+this.body = Bodies.rectangle(x,y,this.w,this.h,options);
+World.add(world , this.body );
+}
+ mostrar()
+ {
+    var pos = this.body.position;
+    rect(pos.x, pos.y, this.w, this.h);
+ }
+}       
+
